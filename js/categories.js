869909bc688d8465b902f63d4c842a37e73bd035
ampi <viltea@udesa.edu.ar>
2024-11-26 17:24:51 -0300

    cambios en css, pagina de registro

---
+++ js/categories.js
@@ -0,0 +1,30 @@
+fetch("https://dummyjson.com/recipes/tags")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let categories = data;
+    let categoriesList = document.querySelector(".categories-list");
+    let categoria = '';
+
+    categoriesList.style.display = "flex"
+    categoriesList.style.flexWrap = "wrap"
+    categoriesList.style.justifyContent = "space-between"
+
+    if (categoriesList) {
+
+    }
+
+    for (let i = 0; i < categories.length; i++) {
+      categoria += `
+                <article>
+                        <a href="./category.html?id=${categories[i].id}">${categories[i]}</a>
+                 </article>
+             `;
+    }
+
+    categoriesList.innerHTML = categoria
+  })
+  .catch(function (error) {
+    console.log("error: ", error);
+  });
